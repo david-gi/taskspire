90ee3a86a07431c80757e00a858467d189f2f46e
david-gi <47605858+david-gi@users.noreply.github.com>
2023-08-17 18:25:52 +0200

    General fine-tuning for style and components

---
+++ src/components/base/DeleteButton.vue
@@ -0,0 +1,40 @@
+<script setup lang='ts'>
+import { ref } from 'vue'
+import DefaultButton from './DefaultButton.vue'
+
+defineEmits<{
+  delete: []
+}>()
+
+const deleteActivated = ref<boolean>()
+
+function startDelete() {
+  setTimeout(() => {
+    deleteActivated.value = true
+    setTimeout(() => deleteActivated.value = false, 3000)
+  },
+    900)
+}
+</script>
+
+<template>
+  <div class="bg-gray">
+    <default-button
+      text="Delete"
+      theme="evil"
+      class="w-16 ring-pink bg-opacity-60 transition duration-300 ease-in-out transform disabled:-translate-x-16"
+      :class="{ 'hidden': deleteActivated }"
+      :active="true"
+      @click="startDelete"
+    />
+    <default-button
+      text="Delete?"
+      theme="mean"
+      class="w-16 animate-pulse -translate-x-16"
+      :class="{ 'hidden': !deleteActivated }"
+      :active="true"
+      @click="$emit('delete')"
+    />
+  </div>
+</template>
+
