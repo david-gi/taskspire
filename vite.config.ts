3b5bdfd5497e64c95cc9807c8a0af3b7e87decde
david-gi <47605858+david-gi@users.noreply.github.com>
2023-11-03 12:19:38 +0100

    Adding server settings to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,13 @@ import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig(<UserConfig>{
   base: process.env.NODE_ENV === 'production' ? './' : '/',
-  // resolve: {
-  //   alias: {
-  //     '@': resolve(__dirname, 'src'),
-  //   },
-  // },
   plugins: [vue(), eslint(), tailwindcss, autoprefixer, tsconfigPaths()],
+  server: {
+    strictPort: true,
+    hmr: {
+      clientPort: 5173
+    }
+  }
 })
