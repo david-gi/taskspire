eb6d7abd13fd8aed0bd1d7d0740edd4f3f5c3e0e
David-Gi <david-gi@gmx.com>
2023-11-03 12:19:37 +0100

    Themeing adjustments

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -17,25 +17,27 @@ function goToHome() {
 <template>
   <div
     v-if="big"
-    class="bg-purple/40 brightness-125 text-center scale-100 py-3"
+    class="bg-blue/25 backdrop-contrast-125 text-center 
+          scale-100 py-6 shadow-lg shadow-gray-light/25"
   >
     <img
-      class="w-20 sm:w-28 mt-0 sm:-mt-14 block sm:inline-block mx-auto rounded-4xl opacity-75 drop-shadow-[-1px_6px_8px_#000]"
+      class="w-20 sm:w-28 mt-0 sm:-mt-14 block sm:inline-block mx-auto rounded-4xl
+             contrast-75 drop-shadow-[-2px_6px_6px_#000]"
       src="../assets/images/logo.png"
     />
     <div class="text-center sm:text-left ml-0 sm:ml-4 pt-0 sm:pt-4 inline-block">
-      <span class="text-green text-4xl font-black">
+      <span class="text-pink text-4xl font-bold">
         {{ $t('website.name') }}
       </span>
       <br />
-      <span class="sm:text-xl text-base font-semibold whitespace-nowrap opacity-90">
+      <span class="sm:text-xl text-pink text-base font-semibold whitespace-nowrap opacity-75">
         {{ $t('website.slogan') }}
       </span>
     </div>
   </div>
   <h1
     v-else
-    class="absolute w-screen text-green mt-2 line-clamp-1 px-0 sm:px-2 md:px-6 lg:px-9 "
+    class="absolute w-screen text-pink mt-2 line-clamp-1 px-0 sm:px-2 md:px-6 lg:px-9 "
   >
     <span class="text-xl ml-1 font-bold">
       {{ $t('website.name') }}
