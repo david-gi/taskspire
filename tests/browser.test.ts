d6926304669daea72f50e638bec7166ab1eeef7a
david-gi <47605858+david-gi@users.noreply.github.com>
2023-11-16 19:11:59 +0100

    Playwright testing with Docker

--- tests/browser.test.ts
+++ tests/browser.test.ts
@@ -1,6 +1,9 @@
-import { test } from '@playwright/test'
+import { Page, test, } from '@playwright/test'
 
-test('browser testing', async ({ page }) => {
-  await page.goto('http://localhost:5173/')
+test('browser testing', async ({ page }, workerInfo) => {
+  await page.goto('http://localhost:5173/#demo')
+  await page.waitForTimeout(1000)
+  await page.setViewportSize({ width: 900, height: 2400 })
+  await page.screenshot({ path: 'playwright-report/' + workerInfo.project.name + '.png', fullPage: true })
   await page.pause()
 })
